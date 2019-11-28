6339250d6820359bfac716e8411323f2542b056a
0632065 <55527062+0632065@users.noreply.github.com>
2019-11-28 14:51:37 +0800

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,43 @@
+interface Shape{
+  abstract double getArea();
+}
+
+class Rectangle implements Shape{
+  private double length;
+  private double width;
+  
+  public Rectangle(double length , double width ){
+    this.length = length;
+    this.weight = weight;
+  }
+  
+  public double getArea(){
+    double ans = length*weight;
+    return ans;
+  }
+  
+  public string toSting(){
+    String ans= double.toString(getArea());
+    return ans;
+  }
+  
+}
+
+class Trianhgle implements Shape{
+  private double base;
+  private double height;
+  
+  public Triangle (double base ,double height){
+    this.base = base;
+    this.height = height;
+  }
+  
+  public double getArea(){
+  
+  }
+  
+  public string toString{
+  
+  }
+}
+
